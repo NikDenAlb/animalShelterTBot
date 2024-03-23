30976249d27c3134b4a6e8ab1188e2eed9e69b30
nikdenalb <ardeniaanderland@gmail.com>
2024-03-23 08:32:35 +0300

    preset correction

--- src/main/java/selfConstructed/animalShelterTBot/service/ShelterService.java
+++ src/main/java/selfConstructed/animalShelterTBot/service/ShelterService.java
@@ -33,8 +33,8 @@ public class ShelterService {
     @PostConstruct
     void init() {
         if (shelterRepository.findAll().isEmpty()) {
-            shelterRepository.save(new Shelter(1L, "Кошки?", 1L, "КукуевоК", "4-5", "Звоните нам",null));
-            shelterRepository.save(new Shelter(2L, "Собаки?", 2L, "КукуевоС", "5-6", "Пишите нам",null));
+            shelterRepository.save(new Shelter(1L, "Кошкин Дом", 1L, "Кукуево", "6:00-18:00", "Звоните нам",null));
+            shelterRepository.save(new Shelter(2L, "Будка Шарика", 2L, "Кукуевка", "9:00-21:00", "Пишите нам",null));
         }
 
     }

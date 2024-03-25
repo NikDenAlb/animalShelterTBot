7657898b59b1bfb478d904573d4404a7a9e7b75a
ivanh <ivanhlop@gmail.com>
2024-03-25 23:03:51 +0300

    Сохранить объект User в БД при старте программы

--- src/main/java/selfConstructed/animalShelterTBot/repository/UserRepository.java
+++ src/main/java/selfConstructed/animalShelterTBot/repository/UserRepository.java
@@ -4,6 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import selfConstructed.animalShelterTBot.model.User;
 
 public interface UserRepository extends JpaRepository<User, String> {
-    Boolean existsByChatIdAndChatIdNotNull(long chatId);
-    User getUserByChatId(long chatId);
+    User findByChatId(long chatId);
 }

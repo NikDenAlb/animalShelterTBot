07f418293beb9fe7bb3cb82a16b2c42861f72a12
nikdenalb <ardeniaanderland@gmail.com>
2024-03-26 10:43:30 +0300

    sql fix and refactor

--- src/main/resources/liquibase/scripts/user.sql
+++ src/main/resources/liquibase/scripts/user.sql
@@ -8,23 +8,24 @@ CREATE TYPE type_user AS enum ('user', 'volunteer');
 CREATE TABLE IF NOT EXISTS reports
 (
     id                  BIGSERIAL PRIMARY KEY,
-    photo VARCHAR[255],
+    photo               VARCHAR[255],
     diet                TEXT,
     change_in_behavior  TEXT,
     general_information TEXT
 );
 CREATE TABLE IF NOT EXISTS pets
 (
-    id       BIGSERIAL PRIMARY KEY,
-    name_pet TEXT   NOT NULL,
-    pet_type type_pet NOT NULL,
-    adult    BIGINT NOT NULL,
-    breed    TEXT   NOT NULL
+    id       BIGSERIAL  PRIMARY KEY,
+    name_pet TEXT       NOT NULL,
+    pet_type type_pet   NOT NULL,
+    adult    BIGINT     NOT NULL,
+    breed    TEXT       NOT NULL
 );
 CREATE TABLE IF NOT EXISTS shelters
 (
     id                  BIGSERIAL PRIMARY KEY,
     name_of_the_shelter TEXT NOT NULL,
+    animal_type         BIGINT NOT NULL,
     address             TEXT NOT NULL,
     opening_hours       TEXT NOT NULL,
     contact_info        TEXT NOT NULL
@@ -32,8 +33,8 @@ CREATE TABLE IF NOT EXISTS shelters
 CREATE TABLE IF NOT EXISTS users
 (
     id           BIGSERIAL PRIMARY KEY,
-    chat_id   BIGINT    NOT NULL,
-    user_type type_user NOT NULL,
+    chat_id      BIGINT    NOT NULL,
+    user_type    type_user NOT NULL,
     full_name    TEXT,
     adult        BIGINT,
     address      TEXT,
